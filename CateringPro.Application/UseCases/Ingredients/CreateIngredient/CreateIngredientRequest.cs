f907b80f742a4e6cc47676bda5428fe3a3c5c716
NoobishLegend <Jessman6@hotmail.com>
2020-07-19 00:20:11 +1000

    Add Request and Response
    CAT-25

---
+++ CateringPro.Application/UseCases/Ingredients/CreateIngredient/CreateIngredientRequest.cs
@@ -0,0 +1,20 @@
+﻿using CateringPro.Domain.Enumerations;
+using MediatR;
+
+namespace CateringPro.Application.UseCases.Ingredients.CreateIngredient
+{
+
+    public class CreateIngredientRequest : IRequest<CreateIngredientResponse>
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public MeasurementType MeasurementType { get; set; }
+
+        public string Name { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
---
+++ CateringPro.Application/UseCases/Ingredients/CreateIngredient/CreateIngredientResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace CateringPro.Application.UseCases.Ingredients.CreateIngredient
+{
+
+    public class CreateIngredientResponse
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public long ID { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
