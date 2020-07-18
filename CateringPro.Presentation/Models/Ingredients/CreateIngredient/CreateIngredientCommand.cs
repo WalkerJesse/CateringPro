3b9006f7c8642a9b72387baae2fb91ff950c7861
NoobishLegend <Jessman6@hotmail.com>
2020-07-19 01:19:36 +1000

    Add Presentation Command and View Model
    CAT-28

---
+++ CateringPro.Presentation/Models/Ingredients/CreateIngredient/CreateIngredientCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace CateringPro.Presentation.Models.Ingredients.CreateIngredient
+{
+
+    public class CreateIngredientCommand
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        /// <summary>
+        /// The type of Measurement the Ingredient uses
+        /// </summary>
+        public string MeasurementType { get; set; }
+
+        /// <summary>
+        /// The Name of the Ingredient
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
