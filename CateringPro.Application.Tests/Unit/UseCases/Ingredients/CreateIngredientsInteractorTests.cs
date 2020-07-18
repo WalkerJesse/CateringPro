d8001abfb05a1dbe4ad4cb948eb4c1104fff12b0
NoobishLegend <Jessman6@hotmail.com>
2020-07-19 00:58:48 +1000

    Add Interactor and Tests
    CAT-26

---
+++ CateringPro.Application.Tests/Unit/UseCases/Ingredients/CreateIngredientsInteractorTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using CateringPro.Application.Services.Persistence;
+using CateringPro.Application.UseCases.Ingredients.CreateIngredient;
+using CateringPro.Domain.Entities;
+using FluentAssertions;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CateringPro.Application.Tests.Unit.UseCases.Ingredients
+{
+
+    public class CreateIngredientsInteractorTests
+    {
+
+        #region - - - - - - Handle Tests - - - - - -
+
+        [Fact]
+        public async Task Handle_ValidRequest_Successful()
+        {
+            // Arrange
+            var _Request = new CreateIngredientRequest();
+            var _CancellationToken = new CancellationToken();
+            var _Ingredient = new Ingredient();
+
+            var _MockMapper = new Mock<IMapper>();
+            _MockMapper
+                .Setup(mock => mock.Map<Ingredient>(_Request))
+                .Returns(_Ingredient);
+
+            var _MockPersistenceContext = new Mock<IPersistenceContext>();
+            var _Interactor = new CreateIngredientInteractor(_MockMapper.Object, _MockPersistenceContext.Object);
+
+            // Act
+            var _Exception = await Record.ExceptionAsync(async () => await _Interactor.Handle(_Request, _CancellationToken));
+
+            // Assert
+            _MockMapper.Verify(mock => mock.Map<Ingredient>(_Request), Times.Once);
+            _MockPersistenceContext.Verify(mock => mock.AddAsync(_Ingredient, _CancellationToken));
+            _MockMapper.Verify(mock => mock.Map<CreateIngredientResponse>(_Ingredient), Times.Once);
+            _Exception.Should().BeNull();
+        }
+
+        #endregion Handle Tests
+
+    }
+
+}
