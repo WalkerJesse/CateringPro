0c4249075fcac17f6b6e0e9292ebed8e09b17536
NoobishLegend <Jessman6@hotmail.com>
2020-07-19 02:10:56 +1000

    Add Presentation Profile and Tests
    CAT-29

---
+++ CateringPro.Presentation.Tests/Unit/Models/Ingredients/CreateIngredient/CreateIngredientProfileTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CateringPro.Presentation.Models.Ingredients.CreateIngredient;
+using Xunit;
+
+namespace CateringPro.Presentation.Tests.Unit.Models.Ingredients.CreateIngredient
+{
+
+    public class CreateIngredientProfileTests
+    {
+
+        #region - - - - - - Profile Configuration Tests - - - - - -
+
+        [Fact]
+        public void ErrorMappingProfile_ConfigurationValidation_Successful()
+        {
+            // Arrange
+            var configuration = new MapperConfiguration(cfg =>
+                cfg.AddProfile<CreateIngredientProfile>());
+
+            // Act
+
+            // Assert
+            configuration.AssertConfigurationIsValid();
+        }
+
+        #endregion Profile Configuration Tests
+
+    }
+
+}
