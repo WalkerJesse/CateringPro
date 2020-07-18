12a70823a934e77bfdc83568c22953c3afd3ab95
NoobishLegend <Jessman6@hotmail.com>
2020-07-18 19:09:20 +1000

    Add Assembly Utilities
    CAT-9

---
+++ CateringPro.Infrastructure/AssemblyUtility.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace CateringPro.Infrastructure
+{
+
+    public class AssemblyUtility
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        public static Assembly GetAssembly() => typeof(AssemblyUtility).Assembly;
+
+        #endregion Methods
+
+    }
+
+}
