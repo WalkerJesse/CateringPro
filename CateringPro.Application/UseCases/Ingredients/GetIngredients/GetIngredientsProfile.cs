d36e077255def4fc3b36e0c2be11a2e34079ce45
NoobishLegend <Jessman6@hotmail.com>
2020-08-16 17:51:36 +1000

    Add Profile and Tests
    CAT-48

---
+++ CateringPro.Application/UseCases/Ingredients/GetIngredients/GetIngredientsProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CateringPro.Domain.Entities;
+using System.Collections.Generic;
+
+namespace CateringPro.Application.UseCases.Ingredients.GetIngredients
+{
+
+    public class GetIngredientsProfile : Profile
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        public GetIngredientsProfile()
+        {
+            _ = this.CreateMap<Ingredient, IngredientDto>()
+                .ForMember(dest => dest.IngredientID, opts => opts.MapFrom(src => src.ID))
+                .ForMember(dest => dest.IngredientName, opts => opts.MapFrom(src => src.Name))
+                .ForMember(dest => dest.MeasurementType, opts => opts.MapFrom(src => src.MeasurementType));
+
+            _ = this.CreateMap<List<IngredientDto>, GetIngredientsResponse>()
+                .ForMember(dest => dest.Ingredients, opts => opts.MapFrom(src => src));
+        }
+
+        #endregion Constructors
+
+    }
+
+}
