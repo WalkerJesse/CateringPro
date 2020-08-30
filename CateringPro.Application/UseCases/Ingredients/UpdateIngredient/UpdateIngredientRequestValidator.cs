e87aeba30540da52c655d7dc906927f241cc2eb3
NoobishLegend <Jessman6@hotmail.com>
2020-08-30 20:39:23 +1000

    Add Request Validation and Tests
    CAT-61

---
+++ CateringPro.Application/UseCases/Ingredients/UpdateIngredient/UpdateIngredientRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace CateringPro.Application.UseCases.Ingredients.UpdateIngredient
+{
+
+    public class UpdateIngredientRequestValidator : AbstractValidator<UpdateIngredientRequest>
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        public UpdateIngredientRequestValidator()
+        {
+            _ = this.RuleFor(i => i.MeasurementType).NotEmpty();
+            _ = this.RuleFor(i => i.Name).MaximumLength(100).NotEmpty();
+        }
+
+        #endregion Constructors
+
+    }
+
+}
