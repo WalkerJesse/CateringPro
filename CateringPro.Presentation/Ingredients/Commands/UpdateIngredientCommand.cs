1108a0805ee2ee74c5873d223c918bda6a66f747
NoobishLegend <Jessman6@hotmail.com>
2020-08-30 20:08:44 +1000

    Add Interface Command and View Model
    CAT-58

---
+++ CateringPro.Presentation/Ingredients/Commands/UpdateIngredientCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace CateringPro.WebApi.Interface.Ingredients.Commands
+{
+
+    /// <summary>
+    /// The Command to update an Ingredient
+    /// </summary>
+    public class UpdateIngredientCommand
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        /// <summary>
+        /// The ID the Ingredient
+        /// </summary>
+        public string IngredientID { get; set; }
+
+        /// <summary>
+        /// The type of Measurement the Ingredient uses
+        /// </summary>
+        public string MeasurementType { get; set; }
+
+        /// <summary>
+        /// The Name of the Ingredient
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
