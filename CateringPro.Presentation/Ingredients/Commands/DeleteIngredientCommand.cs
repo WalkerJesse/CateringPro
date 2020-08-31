407b827838722ccc0b35efcd673c021bf198a22d
NoobishLegend <Jessman6@hotmail.com>
2020-08-31 21:55:34 +1000

    Add Interface Command and View Model
    CAT-66

---
+++ CateringPro.Presentation/Ingredients/Commands/DeleteIngredientCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace CateringPro.WebApi.Interface.Ingredients.Commands
+{
+
+    /// <summary>
+    /// The Command to delete an Ingredient
+    /// </summary>
+    public class DeleteIngredientCommand
+    {
+
+    }
+
+}
