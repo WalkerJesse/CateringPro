db54b4f607aff9a76736b372fab303adf0f19328
NoobishLegend <Jessman6@hotmail.com>
2020-08-31 22:04:37 +1000

    Add Interface Profile and Tests
    CAT-68

---
+++ CateringPro.WebApi.Tests/Unit/Infrastructure/Mappings/Ingredients/DeleteIngredientProfileTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CateringPro.WebApi.Infrastructure.Mappings.Ingredients;
+using Xunit;
+
+namespace CateringPro.WebApi.Tests.Unit.Infrastructure.Mappings.Ingredients
+{
+
+    public class DeleteIngredientProfileTests
+    {
+
+        #region - - - - - - Profile Configuration Tests - - - - - -
+
+        [Fact]
+        public void ErrorMappingProfile_ConfigurationValidation_Successful()
+        {
+            // Arrange
+            var _Configuration = new MapperConfiguration(cfg =>
+                cfg.AddProfile<DeleteIngredientProfile>());
+
+            // Act
+
+            // Assert
+            _Configuration.AssertConfigurationIsValid();
+        }
+
+        #endregion Profile Configuration Tests
+    }
+
+}
