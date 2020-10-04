5cd9c8d738cabf15dfac8c0f9c08b68ff30173dc
NoobishLegend <Jessman6@hotmail.com>
2020-10-04 20:52:24 +1100

    Add RecipesApi
    CAT-79

---
+++ CateringPro.WebApi.Consumer/RecipeApi.cs
@@ -0,0 +1,49 @@
+﻿using CateringPro.WebApi.Interface.Recipes.Commands;
+using CateringPro.WebApi.Interface.Recipes.ViewModels;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CateringPro.WebApi.Consumer
+{
+
+    public class RecipesApi
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly HttpClient m_HttpClient;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public RecipesApi(HttpClient httpClient)
+        {
+            this.m_HttpClient = httpClient ?? throw new System.ArgumentNullException(nameof(httpClient));
+        }
+
+        #endregion Constructors
+
+        #region - - - - - - Methods - - - - - -
+
+        public async Task<ApiResponse<RecipeViewModel>> AddRecipeAsync(CreateRecipeCommand command, CancellationToken cancellationToken)
+        {
+            var _HttpContent = new StringContent(JsonConvert.SerializeObject(command), Encoding.UTF8, "application/json");
+            var _Response = await this.m_HttpClient.PostAsync("api/Recipes/", _HttpContent, cancellationToken);
+            var _ResponseContent = await _Response.Content.ReadAsStringAsync();
+
+            if (_Response.StatusCode == HttpStatusCode.BadRequest)
+                return new ApiResponse<RecipeViewModel>() { ValidationFailure = JsonConvert.DeserializeObject<ValidationFailureResponse>(_ResponseContent) };
+
+            return new ApiResponse<RecipeViewModel>() { Response = JsonConvert.DeserializeObject<RecipeViewModel>(_ResponseContent) };
+        }
+
+        #endregion Methods
+
+    }
+
+}
