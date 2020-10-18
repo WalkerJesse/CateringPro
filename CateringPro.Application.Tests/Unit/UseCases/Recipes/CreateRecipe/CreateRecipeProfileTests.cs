68cb02f297b68fd5d8442947af0e0b05c0fe71b6
NoobishLegend <Jessman6@hotmail.com>
2020-10-18 19:27:16 +1100

    Add Mapping Tests
    CAT-97

--- CateringPro.Application.Tests/Unit/UseCases/Recipes/CreateRecipe/CreateRecipeProfileTests.cs
+++ CateringPro.Application.Tests/Unit/UseCases/Recipes/CreateRecipe/CreateRecipeProfileTests.cs
@@ -1,5 +1,11 @@
 ﻿using AutoMapper;
+using CateringPro.Application.Services.Persistence;
 using CateringPro.Application.UseCases.Recipes.CreateRecipe;
+using CateringPro.Domain.Entities;
+using FluentAssertions;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace CateringPro.Application.Tests.Unit.UseCases.Recipes.CreateRecipe
@@ -15,6 +21,40 @@ namespace CateringPro.Application.Tests.Unit.UseCases.Recipes.CreateRecipe
             => new MapperConfiguration(cfg => cfg.AddProfile<CreateRecipeProfile>()).AssertConfigurationIsValid();
 
         #endregion Profile Configuration Tests
+
+    }
+
+    public class IngredientResolverTests
+    {
+
+        #region - - - - - - Resolve Tests - - - - - -
+
+        [Fact]
+        public void Resolve_ValidID_Successful()
+        {
+            // Arrange
+            var _CancellationToken = CancellationToken.None;
+            var _IngredientID = 1;
+            var _RecipeIngredientDto = new RecipeIngredientDto() { IngredientID = _IngredientID };
+            var _Expected = new Ingredient() { ID = _IngredientID };
+
+            var _MockPersistenceContext = new Mock<IPersistenceContext>();
+            _MockPersistenceContext.Setup(mock => mock.FindAsync<Ingredient>(It.IsAny<object[]>(), _CancellationToken))
+                .Returns(Task.FromResult(_Expected));
+
+            var _Resolver = new IngredientResolver(_MockPersistenceContext.Object);
+
+            // Act
+            var _Actual = _Resolver.Resolve(_RecipeIngredientDto, null, null, null);
+
+            // Assert
+            _Actual.Should().Be(_Expected);
+            _MockPersistenceContext.Verify(mock => mock.FindAsync<Ingredient>(It.IsAny<object[]>(), _CancellationToken));
+            _MockPersistenceContext.VerifyNoOtherCalls();
+
+        }
+
+        #endregion Resolve Tests
 
     }
 
