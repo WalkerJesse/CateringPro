efd77feff4f47b861b5bae7b891f24ec73707d4b
NoobishLegend <Jessman6@hotmail.com>
2020-10-18 13:49:02 +1100

    Add Interface Mappings and Tests
    CAT-89

---
+++ CateringPro.WebApi/Infrastructure/Mappings/Recipes/GetRecipesProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CateringPro.Application.UseCases.Recipes.GetRecipes;
+using CateringPro.WebApi.Interface.Recipes.Queries;
+using CateringPro.WebApi.Interface.Recipes.ViewModels;
+
+namespace CateringPro.WebApi.Infrastructure.Mappings.Recipes
+{
+
+    public class GetRecipesProfile : Profile
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        public GetRecipesProfile()
+        {
+            _ = this.CreateMap<RecipeDto, RecipeViewModel>();
+
+            _ = this.CreateMap<GetRecipesQuery, GetRecipesRequest>();
+
+            _ = this.CreateMap<GetRecipesResponse, RecipesViewModel>();
+        }
+
+        #endregion Constructors
+
+    }
+
+}
