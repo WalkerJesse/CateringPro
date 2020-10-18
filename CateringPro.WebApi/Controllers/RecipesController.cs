657da1cafbef2dabdf5bc2e9c36a5af4868692d9
NoobishLegend <Jessman6@hotmail.com>
2020-10-18 13:50:30 +1100

    Add Controller Action
    CAT-90

--- CateringPro.WebApi/Controllers/RecipesController.cs
+++ CateringPro.WebApi/Controllers/RecipesController.cs
@@ -1,6 +1,8 @@
 ﻿using CateringPro.Application.UseCases.Recipes.CreateRecipe;
+using CateringPro.Application.UseCases.Recipes.GetRecipes;
 using CateringPro.Common.CodeContracts;
 using CateringPro.WebApi.Interface.Recipes.Commands;
+using CateringPro.WebApi.Interface.Recipes.Queries;
 using CateringPro.WebApi.Interface.Recipes.ViewModels;
 using CateringPro.WebApi.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -36,6 +38,12 @@ namespace CateringPro.WebApi.Controllers
         [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
         public Task<IActionResult> CreateRecipe([FromBody] CreateRecipeCommand command)
             => this.m_ControllerAction.CreateAsync<RecipeViewModel, CreateRecipeRequest, CreateRecipeResponse>(command, CancellationToken.None);
+
+        [HttpGet]
+        [ProducesResponseType(typeof(RecipesViewModel), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        public Task<IActionResult> GetRecipes(GetRecipesQuery query)
+            => this.m_ControllerAction.ReadAsync<RecipesViewModel, GetRecipesRequest, GetRecipesResponse>(query, CancellationToken.None);
 
         #endregion Methods
 
