ffb1561745736dcb87cb5c169cfa341c83b29eca
NoobishLegend <Jessman6@hotmail.com>
2022-03-03 23:41:47 +1100

    Add Commands

---
+++ CateringPro.WebApi/Presentation/Commands/Ingredients/CreateIngredientCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace CateringPro.WebApi.Presentation.Commands.Ingredients
+{
+
+    /// <summary>
+    /// The Command to create an Ingredient
+    /// </summary>
+    public class CreateIngredientCommand
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        /// <summary>
+        /// The Name of the Ingredient
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
---
+++ CateringPro.WebApi/Presentation/Commands/Ingredients/UpdateIngredientCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CateringPro.WebApi.Presentation.Commands.Ingredients
+{
+
+    /// <summary>
+    /// The Command to update an Ingredient
+    /// </summary>
+    public class UpdateIngredientCommand
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        [JsonIgnore]
+        public long IngredientID { get; set; }
+
+        /// <summary>
+        /// The Name of the Ingredient
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
