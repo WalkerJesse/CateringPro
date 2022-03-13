9cd9e6c02eee046690aaf705f5cd6016abcb369d
NoobishLegend <Jessman6@hotmail.com>
2022-03-13 20:52:14 +1100

    Add input port and outputport

---
+++ CateringPro.Application/UseCases/Recipes/DeleteRecipe/DeleteRecipeInputPort.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Mediator;
+
+namespace CateringPro.Application.UseCases.Recipes.DeleteRecipe
+{
+
+    public class DeleteRecipeInputPort : IUseCaseInputPort<IDeleteRecipeOutputPort>
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public long RecipeID { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
---
+++ CateringPro.Application/UseCases/Recipes/DeleteRecipe/IDeleteRecipeOutputPort.cs
@@ -0,0 +1,22 @@
+﻿using CateringPro.Application.Dtos;
+using CleanArchitecture.Mediator;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CateringPro.Application.UseCases.Recipes.DeleteRecipe
+{
+
+    public interface IDeleteRecipeOutputPort : IAuthenticationOutputPort
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        Task PresentDeletedRecipeAsync(RecipeDto recipe, CancellationToken cancellationToken);
+
+        Task PresentRecipeNotFound(long recipeID, CancellationToken cancellationToken);
+
+        #endregion Properties
+
+    }
+
+}
