d7a5555fcb64e2cafe1480d5dc793f22b4b35a15
NoobishLegend <Jessman6@hotmail.com>
2022-03-13 21:04:47 +1100

    Add Controller Action

--- CateringPro.InterfaceAdapters/Controllers/RecipeController.cs
+++ CateringPro.InterfaceAdapters/Controllers/RecipeController.cs
@@ -1,4 +1,5 @@
 ﻿using CateringPro.Application.UseCases.Recipes.CreateRecipe;
+using CateringPro.Application.UseCases.Recipes.DeleteRecipe;
 using CateringPro.Application.UseCases.Recipes.GetRecipes;
 using CleanArchitecture.Mediator;
 
@@ -24,6 +25,9 @@ namespace CateringPro.InterfaceAdapters.Controllers
         #region - - - - - - Methods - - - - - -
 
         public Task CreateRecipeAsync(CreateRecipeInputPort inputPort, ICreateRecipeOutputPort outputPort, CancellationToken cancellationToken)
+            => this.m_UseCaseInvoker.InvokeUseCaseAsync(inputPort, outputPort, cancellationToken);
+
+        public Task DeleteRecipeAsync(DeleteRecipeInputPort inputPort, IDeleteRecipeOutputPort outputPort, CancellationToken cancellationToken)
             => this.m_UseCaseInvoker.InvokeUseCaseAsync(inputPort, outputPort, cancellationToken);
 
         public Task GetRecipesAsync(IGetRecipesOutputPort outputPort, CancellationToken cancellationToken)
